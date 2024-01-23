c42373871549628491068fec7f262c937bd5d3d7
Eric Tran <eric.tran.11.01.1999@gmail.com>
2024-01-23 19:37:02 +1100

    updating files with constant new lines with more imporvement

---
+++ inputcode.js
@@ -0,0 +1,126 @@
+
+let myChart;
+
+function displayStatsChart(pokemonData) {
+    const stats = pokemonData.stats;
+
+    const labels = stats.map(stat => stat.name);
+    const values = stats.map(stat => stat.base_stat);
+
+    const ctx = document.getElementById('pokemonStatsChart').getContext('2d');
+    
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                
+                data: values,
+                backgroundColor: ['rgba(255, 0, 0, 0.8)', 'rgba(255, 93, 0, 0.8)', 'rgba(255, 208, 0, 0.8)', 
+                'rgba(85, 133, 255, 0.8)', 'rgba(117, 227, 97, 0.8)', 'rgba(250, 85, 155, 0.8)'],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: false,
+                    suggestedMin: 0,
+                    suggestedMax: 120
+                }
+            },
+
+                plugins: {
+                    legend: {display: false },
+                    title: {
+                    display: true,
+                    text: 'Pokemon Base Stat',
+                    position: 'top',
+                    font: {
+                        size: 16,
+                        weight: 'bold'
+                    }
+                }
+            }
+        }
+    });
+}
+
+function ExtractPokemonData() {
+    const inputElement = document.getElementById("pokemonIdInput");
+    const userInput = inputElement.value;
+
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${userInput}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const pokemonData = {
+                name: data.name,
+                id: data.id,
+                height: data.height,
+                weight: data.weight,
+                stats: data.stats.map(stat => ({
+                    name: stat.stat.name,
+                    base_stat: stat.base_stat
+                }))
+            };
+
+            
+            // console.log(pokemonData);
+            updateStatsChart(pokemonData);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+function updateStatsChart(pokemonData) {
+    const stats = pokemonData.stats;
+
+    const labels = stats.map(stat => stat.name);
+    const values = stats.map(stat => stat.base_stat);
+
+    if (myChart) {
+        myChart.data.labels = labels;
+        myChart.data.datasets[0].data = values;
+
+        myChart.options.aspectRatio = 2;
+
+        myChart.update();
+    } else {
+        const ctx = document.getElementById('pokemonStatsChart').getContext('2d');
+        myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    data: values,
+                    backgroundColor: ['rgba(255, 0, 0, 0.8)', 'rgba(255, 93, 0, 0.8)', 'rgba(255, 208, 0, 0.8)', 
+                    'rgba(85, 133, 255, 0.8)', 'rgba(117, 227, 97, 0.8)', 'rgba(250, 85, 155, 0.8)'],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                aspectRatio: 2,
+                scales: {
+                    y: {
+                        beginAtZero: false,
+                        suggestedMin: 0,
+                        suggestedMax: 120
+                    }  
+                },
+                plugins: {
+                    legend: {display: false},
+                title: {
+                    display: true,
+                    text: 'Pokemon Base Stat',
+                    position: 'top',
+                    font: {
+                        size: 16,
+                        weight: 'bold'
+                    }
+                }
+            }
+        }
+    });
+    }
+}
